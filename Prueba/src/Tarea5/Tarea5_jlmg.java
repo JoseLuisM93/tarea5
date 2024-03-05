c6313b75ea81bdf8d6fd7cf0eee0b81601ad0892
Usuario <Usuario@DESKTOP-6HHMIHR>
2024-03-05 20:41:22 +0100

    Mi primer COMMIT

---
+++ Prueba/src/Tarea5/Tarea5_jlmg.java
@@ -0,0 +1,8 @@
+package Tarea5;
+
+public class Tarea5_jlmg {
+    public static void main(String[] args) {
+        System.out.println("¡Hola, Mundo!");
+        System.out.println("Mi nombre es Jose Luis Martínez García.");
+    }
+}
